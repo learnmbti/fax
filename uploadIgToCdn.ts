ad450eaf89cfcc3f28cfd27d2a23324408395af3
random-istp <random.istp@gmail.com>
2024-02-16 22:29:49 +0800

    write file

--- uploadIgToCdn.ts
+++ uploadIgToCdn.ts
@@ -1,5 +1,8 @@
+import { writeFileSync } from "fs"
+
 const authHeader = process.env.CMS_AUTH
 const cmsUrl = process.env.CMS_URL
 
 const response = await fetch(`${cmsUrl}/ig/posts`, {headers: {Authorization: authHeader}})
-console.log(await response.text())
+const json = await response.text()
+writeFileSync('cdn/posts.json', json)
