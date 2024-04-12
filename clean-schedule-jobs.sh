b9878ba48e55bcf83d9c9e990cca959a0568131d
random-istp <random.istp@gmail.com>
2024-04-12 22:36:55 +0800

    clean schedule jobs

---
+++ clean-schedule-jobs.sh
@@ -0,0 +1,10 @@
+token=$API_TOKEN_GITHUB
+repo=learnmbti/fax
+
+ids=$(curl -s -H "Authorization: token $token" \
+     https://api.github.com/repos/$repo/actions/runs | \
+     jq '.workflow_runs[] | select([.status] | inside(["in_progress", "queued"])) | .id')
+set -- $ids
+for i; do curl \
+     -H "Authorization: token $token" \
+     -X POST "https://api.github.com/repos/$repo/actions/runs/$i/cancel"; done
